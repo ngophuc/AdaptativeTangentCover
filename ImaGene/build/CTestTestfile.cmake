8bfaad89cb5bc62de99ea134b25d65d75d3f9806
ngophuc <ngo.diemphuc@gmail.com>
2021-05-21 11:00:50 +0200

    simplify IPOL

--- ImaGene/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/hngo/Downloads/AdaptiveTangentCover/ImaGene
-# Build directory: /Users/hngo/Downloads/AdaptiveTangentCover/ImaGene/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("include")
-subdirs("tests")
--- ImaGene/build/tests/TestCompNoiseDetect/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/hngo/Downloads/AdaptiveTangentCover/ImaGene/tests/TestCompNoiseDetect
-# Build directory: /Users/hngo/Downloads/AdaptiveTangentCover/ImaGene/build/tests/TestCompNoiseDetect
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
